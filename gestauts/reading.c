75aec33eb1015af50c82915fa1fecf0df3921930
Fernando Mendes <devfjmendes@gmail.com>
2014-03-23 13:26:49 +0000

    Initial reading module draft
    Tokens are being read, but there's no functions to do something with them. Currently waiting for data structures to be implemented, so we can add info extracting functions.

--- gestauts/reading.c
+++ gestauts/reading.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
+static void extract_year_info(char* year_str) {
+	/* Function that should use year to complete statistics */
+	int year = atoi(year_str);
+	return 0;
+}
 
-void extract_info(char* buffer) {
+static void extract_author_info(char *author) {
+	/* Function that should:
+	 * add author to author_index
+	 * user author info to calculate statistics
+	 */
+	return 0;
+}
+
+static void tokenize(char* buffer) {
+	char *token = strtok(buffer, ",");
+	
+	while (token) {
+		printf("%s ", token);
+		token = strtrim( strtok(NULL, ",") );
+		isalpha(token[0]) ? extract_author_info(token) : extract_year_info(token);
+	}
 }
 
 int read_from_file(char* filename) {
@@ -15,7 +36,7 @@ int read_from_file(char* filename) {
 
 	while( fgets(buffer, 1024, file) ) {
 		printf("%s\n", buffer);
-		extract_info(buffer);
+		tokenize(buffer);
 	}
 
 	return 0;
@@ -26,7 +47,7 @@ int main() {
 	int result = read_from_file("./publicx.txt");
 	
 	if (result == -1)
-		printf("FILE DOESN'T EXIST");
+		printf("FILE DOESN'T EXIST\n");
 
 	return 0;
 }
