c6111f5dd06721cbd3a4d8654d0b8c307c4cab56
Fernando Mendes <devfjmendes@gmail.com>
2014-04-04 10:54:42 +0100

    Created AVL test for strings
    No particular feature is tested, just trying to replicate a bug with a
    module in the app itself

---
+++ tests/avl_string.c
@@ -0,0 +1,55 @@
+#include "../gestauts/lib/headers/avl.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char* String;
+
+AVL_DEF(String, String)
+
+String cloneString(String str) {
+	int size = strlen(str) + 1;
+
+	String clone = (String)malloc(sizeof(char) * size);
+	strncpy(clone, str, size);
+
+	return clone;
+}
+
+int compareString(String* n, String* a, String b) {
+	String key = n ? (*n) : (*a);
+	int cmp = strcmp(key, b);
+	
+	printf("\nCOMPARED >>> %s <<< WITH >>> %s <<< AND GOT %d", key, b, cmp);
+
+	return cmp;
+}
+
+
+void printStringAVL(StringAVLNode t) {
+	if (t) {
+		if ( avlStringGetLeftChild(t) )
+			printStringAVL( avlStringGetLeftChild(t) );
+		
+		printf("%s\n", t -> content);
+
+		if ( avlStringGetRightChild(t) )
+			printStringAVL( avlStringGetRightChild(t) );
+	}
+}
+
+int main() {
+	StringAVL t = avlNew(String, &compareString, NULL, NULL, &cloneString);
+
+	avlStringInsert(t, "ABC");
+	avlStringInsert(t, "Abacate");
+	avlStringInsert(t, "Abc Def");
+	avlStringInsert(t, "A lot of fruits");
+	avlStringInsert(t, "Ananas");
+	avlStringInsert(t, "Acb Fed");
+	avlStringInsert(t, "Abacaxi");
+	
+	putchar('\n');
+	putchar('\n');
+	printStringAVL( t->root );
+}
