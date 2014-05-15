32047816616183c03c32de91cbc14e2e3fdc9c25
Fernando Mendes <devfrmendes@gmail.com>
2014-05-15 22:00:57 +0100

    Makes a really small draft of AdminUser

---
+++ src/AdminUser.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author frmendes
+ */
+public class AdminUser extends BasicUser {
+
+    public AdminUser() {
+        super();
+    }
+    
+    public AdminUser( String name, String password, String email ) {
+        super(name, password, email);
+    }
+    
+    public AdminUser( AdminUser a ) {
+        super(a);
+    }
+    
+    public String toString() {
+        return "### Admin: ###" + super.toString();
+    }
+    
+}
