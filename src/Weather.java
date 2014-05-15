ac5c0e5ef771e6a5bb388067ea528554530183a6
Fernando Mendes <devfrmendes@gmail.com>
2014-05-15 20:02:18 +0100

    Fixes negative weather bug

--- src/Weather.java
+++ src/Weather.java
@@ -8,10 +8,10 @@ import java.util.Arrays;
 public class Weather {
     private int weatherIndex;
     
-    private static final String[] weatherStates = {"Indoor", "Clear Sky", "Cloudy", "Light Rain", "Heavy Rain", "Snowing", "Hailing", "Windy", "Stormy"};
+    private static final String[] weatherStates = {"No Information Available", "Clear Sky", "Cloudy", "Light Rain", "Heavy Rain", "Snowing", "Hailing", "Windy", "Stormy"};
     
     public Weather() {
-        this.weatherIndex = -1;
+        this.weatherIndex = 0;
     }
     
     public Weather(int index) {
@@ -62,6 +62,6 @@ public class Weather {
     }
     
     private static int getIndexOf(String weather) {
-        return Arrays.asList(Weather.weatherStates).indexOf(weather);
+        return Math.max( Arrays.asList(Weather.weatherStates).indexOf(weather), 0);
     }
 }
