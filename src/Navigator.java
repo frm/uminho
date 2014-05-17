448bce9739afd2ba9f40ac332166c59b677676bb
Fernando Mendes <devfrmendes@gmail.com>
2014-05-17 14:28:39 +0100

    Fixes navigator bugs, changes implementation

---
+++ src/Navigator.java
@@ -0,0 +1,191 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ *
+ * @author frmendes
+ */
+public abstract class Navigator<T> {
+    
+    private int navigator;
+    private boolean end;
+    private boolean navigating;
+    private ArrayList<T> list;
+    private int lastOption;
+    private static Prompt[] optionsParser;
+    
+    private static final int NumberDisplays = 10;
+    
+    public Navigator() {
+        this.navigator = 0;
+        this.lastOption = -1;
+        this.end = false;
+        this.navigating = false;
+        this.list = new ArrayList<T>();
+        this.optionsParser = Navigator.optionsGenerator(this);
+    }
+    
+    public Navigator(ArrayList<T> list) {
+        this.navigator = 0;
+        this.lastOption = -1;
+        this.end = false;
+        this.navigating = false;
+        this.list = (ArrayList<T>) list.clone();
+        this.optionsParser = Navigator.optionsGenerator(this);
+    }
+    
+    public abstract void select(T t);
+    public abstract void print(T t);
+    public abstract String emptyMessage();
+    
+    
+    public int getLastOption() {
+        return this.lastOption;
+    }
+    
+    private boolean reachedEnd() {
+        return this.end;
+    }
+    
+    private boolean isNavigating() {
+        return this.navigating;
+    }
+    
+    private void startNavigating() {
+        this.navigating = true;
+    }
+    
+    private void quit() {
+        this.navigating = false;
+    }
+    
+    public void reachEnd() {
+        this.end = true;
+    }
+    
+    private void next() {        
+        int limit = this.navigator + Navigator.NumberDisplays;
+        Iterator<T> it = this.list.iterator();
+        
+        while ( it.hasNext() && this.navigator < limit) {
+            System.out.print(++this.navigator + ". ");
+            this.print( it.next() );
+        }
+        
+        if (this.navigator == 0) {
+            System.out.println( this.emptyMessage() );
+            FitnessUM.pressEnterToContinue();
+            this.quit();
+        }
+        
+        else if (this.navigator < limit)
+            this.reachEnd();
+              
+    }
+    
+    private void backtrace() {
+        int i = this.navigator - Navigator.NumberDisplays;
+        
+        while ( i < this.navigator ) {
+            System.out.print(i + 1);
+            this.print( this.list.get(i++) );
+        }
+    }
+    
+    public void navigate() {
+        this.startNavigating();
+        this.next();
+        
+        while( this.isNavigating() ) {
+            if ( this.reachedEnd() )
+                if ( this.navigator < Navigator.NumberDisplays )         this.optionsParser[0].exec();
+                else                                                                                 this.optionsParser[2].exec();
+            else
+                if ( this.navigator == Navigator.NumberDisplays )      this.optionsParser[1].exec();
+                else                                                                                 this.optionsParser[3].exec();
+        }
+    }
+    
+    private void invalidOption(int permission) {
+        System.out.println("Invalid option");
+        this.optionsParser[permission].exec();
+    }
+
+    private void parseOptionsPermit(int permission) {
+        char c = new Scanner(System.in).nextLine().charAt(0);
+        if ( Character.isDigit(c) )
+            this.actionOptions(permission, c);                    
+        else
+           this.navigationOptions(permission, c);
+    }
+    
+    private void actionOptions(int permission, char c) {
+        int option = -1;
+        try {
+            option = Navigator.parseIntInRange(this.getMinimumOption(), this.navigator, c);
+        } catch (IllegalArgumentException e) {
+            this.invalidOption(permission);
+            return;
+        }
+            
+            this.lastOption = option - 1;
+            this.select( this.list.get(this.lastOption) );
+    }
+    
+    private void navigationOptions(int permission, char option) {
+        char c = Character.toLowerCase(option);
+        switch (c) {
+                case 'q':
+                    this.quit();
+                    break;
+                case 'c':
+                    if(permission == 1 || permission == 3) this.next();
+                    else this.invalidOption(permission);
+                    break;
+                case 'b':
+                        if (permission == 2 || permission == 3) this.backtrace();
+                        else this.invalidOption(permission);
+                        break;
+                default:
+                    this.invalidOption(permission);
+                    break;                    
+        }
+    }
+    
+    private int getMinimumOption() {
+        return Math.max(1, this.navigator - Navigator.NumberDisplays - 1);
+    }
+    
+    // For a few seconds I felt like I was writing JavaScript
+    private static Prompt[] optionsGenerator(final Navigator n) {
+        return new Prompt[] {
+            new Prompt() { public void exec() {
+                System.out.println("\nNo more options available. Press 'q' to quit.\nEnter an option number to select it.");
+                n.parseOptionsPermit(0);
+            }},
+             new Prompt() { public void exec() {
+                System.out.println("\nPress 'c' to continue reading, 'q' to quit.\nEnter an option number to select it.");
+                n.parseOptionsPermit(1);
+              }},
+             new Prompt() { public void exec() {
+                 System.out.println("\nNo more options available. Press 'q' to quit, 'b' to backtrace.\nEnter an option number to select it.");
+                 n.parseOptionsPermit(2);
+             }},
+             new Prompt() { public void exec() {
+                System.out.println("\nPress 'c' to continue reading, 'b' to backtrace and 'q' to quit.\nEnter an option number to select it");
+                n.parseOptionsPermit(3);
+             }}                
+        };
+    }
+    
+    private static int parseIntInRange(int min, int max, char c) throws IllegalArgumentException {
+        int val = Character.getNumericValue(c);
+        
+        if (val < min || val > max)
+            throw new IllegalArgumentException("Value off range");
+        
+        return val;
+    }
+}
