4b08d20181b8a8f03f5f3499975ff44c1fd4873f
Fernando Mendes <devfjmendes@gmail.com>
2014-05-18 19:45:56 +0100

    Adds name entry structure

--- src/UserDatabase.java
+++ src/UserDatabase.java
@@ -13,6 +13,7 @@ public class UserDatabase {
     // Both HashMaps refer to the same user, pointer is shared
     private HashMap<Integer, User> idEntry;              // User indexation by ID
     private HashMap<String, User> emailEntry;          // User indexation by email
+    private HashMap<String, User> nameEntry;          // User indexation by name
     private int userCount;                                          // Total number of users that have been saved
 
     /** Empty constructor
@@ -20,6 +21,7 @@ public class UserDatabase {
     public UserDatabase() {
         this.idEntry = new HashMap<Integer, User>();
         this.emailEntry = new HashMap<String, User>();
+        this.nameEntry = new HashMap<String, User>();
         this.userCount = 0;
     }
 
@@ -31,9 +33,12 @@ public class UserDatabase {
         this.userCount = db.nrUsers();
         
         this.emailEntry = new HashMap<String, User>();
+        this.nameEntry = new HashMap<String, User>();
         
-        for (User u : this.idEntry.values() )
+        for (User u : this.idEntry.values() ) {
             this.emailEntry.put( u.getEmail(), u);
+            this.nameEntry.put( u.getName(), u);
+        }
         
     }
     
@@ -93,6 +98,17 @@ public class UserDatabase {
         
         return u;
     }
+    
+    public User findByName(String name) {
+        User u = new User();
+        try {
+            u = this.nameEntry.get(name).clone();
+        } catch (Exception e) {
+            u = null;
+        }
+        
+        return u;
+    }
 
     /** Either saves or updates a user
      * If the user id is < 0, it wasn't created and we need to give it an id, otherwise, we override it
@@ -107,6 +123,7 @@ public class UserDatabase {
                 
         this.idEntry.put( newUser.getId(), newUser );
         this.emailEntry.put( newUser.getEmail(), newUser );
+        this.nameEntry.put ( newUser.getName(), newUser );
     }
     
     /** Deletes a user from the network
@@ -114,9 +131,10 @@ public class UserDatabase {
      * @param userId ID of the to delete
      */
     public void delete(int userId) {
-        String email = this.findById(userId).getEmail();
+        User u = this.findById(userId);
         this.idEntry.remove(userId);
-        this.emailEntry.remove(email);
+        this.emailEntry.remove( u.getEmail() );
+        this.nameEntry.remove( u.getName() );
     }
 
     @Override
