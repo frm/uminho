27922a60a50e3a25af30e5ce3251cc720ed2ec77
Fernando Mendes <devfjmendes@gmail.com>
2014-05-22 13:01:32 +0100

    Adds extra string functions that may come in handy

---
+++ include/strutil.c
@@ -0,0 +1,41 @@
+#include "strutil.h"
+
+static int new_str_end (char *str) {
+    int i = strlen(str) - 1;
+
+    while ( i > 0 && isspace(str[i]) )
+        i--;
+
+    return i + 1;
+}
+
+static char* new_str_start (char *str) {
+    while ( *str != '\0' && isspace(*str) )
+        str++;
+
+    return str;
+}
+
+char* strtrim(char *str) {
+    int size;
+    char *buffer, *new_str;
+
+    if (!str || !*str)
+        return NULL;
+
+    new_str = new_str_start(str);
+    size = new_str_end(new_str);
+
+    buffer = (char*)malloc(size + 1);
+    memcpy(buffer, new_str, size);
+    buffer[size] = '\0';
+
+    return buffer;
+}
+
+char* str_dup(char* str) {
+    int size = strlen(str) + 1; /* avoiding multiple function calls */
+    char* dup = (char*)malloc(size);
+    memcpy(dup, str, size);
+    return dup;
+}
