7534431e9add475ceabed90ac03e92788735f179
Fernando Mendes <devfjmendes@gmail.com>
2014-05-26 00:31:52 +0100

    New test version

--- test/pipe_test/double_writer.c
+++ test/pipe_test/double_writer.c
@@ -3,47 +3,44 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 
 int main() {
     mkfifo("PIPE", 0666);
 
-    int pid1 = fork();
     int pid2;
-    int fd = open("PIPE", O_WRONLY);
-    int* status;
+    int fd = open("PIPE", O_WRONLY); // Open pipe
+    int* status; // placeholder
+    int file = open("./text.txt", O_CREAT | O_TRUNC | O_WRONLY );    // Create a file for printing the pipe output
+    int pid1 = fork(); // Creates first pipe for writing to file
 
     if (!pid1) {
-        pid2 = fork();
+
+        pid2 = fork(); // Creates second pipe for concurrent writing to PIPE
 
         if (!pid2) {
-            char a = 'a';
-            for (int i = 0; i < 1024; i++)
-                write( fd, &a, sizeof(char) );
-            _exit(1);
+            char* str128 = "I HAVE TO WRITE 100 CHARS HERE SO LET ME SEE... WAIT, BETTER MAKE IT 128. MAN, THIS FEELS LIKE TWITTER BUT THE OPPOSITE. ABCDEF";
+            write( file, str128, sizeof(str128) );              // write 128b to PIPE
+            _exit(1);                                         // Dude, kill yourself
         }
 
         else {
-            char b = 'b';
-            char nd = '\0';
-            for (int i = 0; i < 1024; i++)
-                write( fd, &b, sizeof(char) );
+            char* mystr = "THIS STRING IS NOT LIKE THE OTHER ONE. IT IS MUCH BETTER. AND IT IS GETTING BETTER ALL THE TIME SHUT UP RINGO NO ONE LIKES YOU.";
+            write( file, mystr, sizeof(mystr) );              // write 128b to PIPE
 
-            wait(status);
-            write(fd, &nd, sizeof(char) );
-            close(fd);
+            wait(status);                               // wait for your kid
+            close(fd);                                  // Shut your pipe
         }
 
-        _exit(1);
+        _exit(1);                                       // And now it's time to die
      }
 
      else {
-        int file = open("text.txt", O_CREAT | O_TRUNC | O_WRONLY );
-        char buf;
+        char buf[1024];
+        int file = open("./text.txt", O_CREAT | O_TRUNC | O_WRONLY );    // Create a file for printing the pipe output
 
-        while( read( fd, &buf, sizeof(char) ) ) {
-            write(file, &buf, 1024);
-            if(buf == '\0') break;
-        }
+        read(fd, buf, 1024);
+        write( file, buf, strlen(buf) );
 
         close(fd);
         close(file);
