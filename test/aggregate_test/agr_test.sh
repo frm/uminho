7acfa11c4e8ef72543be31755748e077e3bd16f5
Fernando Mendes <devfrmendes@gmail.com>
2014-05-30 22:20:16 +0100

    Prettifying test

--- test/aggregate_test/agr_test.sh
+++ test/aggregate_test/agr_test.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+rm a.out
+rm *.dat
+
 clang -Wall -Wextra -pedantic -g -DDEBUG ../../src/aggregation.c ../../src/aggregate.c ./aggregate_test.c
 ./a.out
 
 for i in {1..8}
 do
 	if [ -f "$i.dat" ]
-		then cat "$i.dat"
+		then
+			echo "------ $i.dat ------"
+			cat "$i.dat"
 	fi
 done
