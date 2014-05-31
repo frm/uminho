1ea33d5d6191a737b33bf8e8bb60fb1ef045a109
Fernando Mendes <devfrmendes@gmail.com>
2014-06-01 00:14:31 +0100

    Fixes leaks

--- src/server_lib.c
+++ src/server_lib.c
@@ -11,11 +11,15 @@ int incrementar(char* prefix[], int value){
 	if(!prefix) return -1;
 	char count[10];
 	sprintf(count, "%d", value);
-	char* new_str = (char*) calloc( strlen( prefix[0] ) + strlen(count) + 2, sizeof(char) ); // 4 => '\0', ';' and "2"
-	sprintf(new_str, "%s;%s%s;", prefix[0], "2", count);
 
-	new_str = (char*)realloc( new_str, strlen(new_str) + strlen( prefix[1] ) + 1 ); // 1 => '\0'
-	sprintf(new_str, "%s%s", new_str, prefix[1]);
+	char* new_str = (char*) calloc(
+	strlen( prefix[0] ) +
+	strlen( prefix[1] ) +
+	strlen(count) +
+	4,
+	sizeof(char)
+	); // 4 => '\0', ';' and "2"
+	sprintf(new_str, "%s;%s%s;%s", prefix[0], "2", count, prefix[1]);
 
 	for(int i = 2; prefix[i]; i++){
 		new_str = (char*)realloc( new_str, strlen(new_str) + strlen( prefix[i] ) + 2 ); // 2 => '\0' and :
@@ -37,11 +41,16 @@ int agregar(char *prefix[], unsigned level, char *path){
 	if(!prefix) return -1;
 	char lvl[10];
 	sprintf(lvl, "%d", level);
-	char* new_str = (char*) calloc( strlen( prefix[0] ) + strlen(path) + strlen(lvl) + 4, sizeof(char) ); // 4 => ;, '\0' and "3"
-	sprintf(new_str, "%s;%s%s;%s", prefix[0], "3", lvl, path);
 
-	new_str = (char*)realloc( new_str, strlen(new_str) + strlen( prefix[1] ) + 2 ); // 2 => ; and '\0'
-	sprintf(new_str, "%s;%s", new_str, prefix[1]);
+	char* new_str = (char*) calloc(
+		strlen( prefix[0] ) +
+		strlen( prefix[1] ) +
+		strlen(path) +
+		strlen(lvl) +
+		5,
+		sizeof(char)
+		); // 5 => ;, '\0' and "3"
+	sprintf(new_str, "%s;%s%s;%s;%s", prefix[0], "3", lvl, path, prefix[1]);
 
 	for(int i = 2; prefix[i]; i++){
 		new_str = (char*)realloc( new_str, strlen(new_str) + strlen( prefix[i] ) + 2 ); // 2 => ; and '\0'
