cd774231ddc22b7e441afc718f2f2925149db4b7
Fernando Mendes <devfrmendes@gmail.com>
2014-06-01 17:56:14 +0100

    Adds pipe writing to server_lib

--- src/server_lib.c
+++ src/server_lib.c
@@ -7,12 +7,32 @@
 
 #include "server.h"
 
+static void write_to_pipe(char* str) {
+	int fd = open(SERVER_NAME, O_WRONLY | O_APPEND);
+	write(fd, str, strlen(str) + 1);
+	close(fd);
+}
+
+static void append_prefix(char** str, char* prefix, int offset) {
+	*str = (char*)realloc(*str, offset + strlen(*str) + strlen(prefix) );
+	sprintf(*str, "%s%s", *str, prefix);
+}
+
+static void append_prefixes(char** str, char* prefix[]) {
+	for(int i = 1; prefix[i]; i++) {
+		if (i == 1)
+			append_prefix(str, prefix[i], 1);
+		else
+			append_prefix(str, prefix[i], 2);
+	}
+}
+
+
 int incrementar(char* prefix[], int value) {
 	if(!prefix) return -1;
 
 	char count[10];
 	sprintf(count, "%d", value);
-	printf("SIZE OF COUNT %lu\n", strlen(count));
 
 	char* new_str = (char*)calloc(
 		strlen(prefix[0]) +
@@ -22,41 +42,21 @@ int incrementar(char* prefix[], int value) {
 		);
 
 	sprintf(new_str, "%s;2%s;", prefix[0], count);
-	printf("SIZE EXPECTED: %lu\n", strlen(prefix[0]) + strlen(count) + 4);
-	printf("NEW_STR: %s WITH SIZE: %lu\n", new_str, strlen(new_str) + 1);
-	printf("IF LAST CHAR IS NULL, THEN YES WILL BE PRINT: ");
-	if (new_str[strlen(new_str)] == '\0') printf("YES\n");
-	else printf("%c", new_str[strlen(new_str)]);
 
-	for(int i = 1; prefix[i]; i++) {
-		if (i == 1) {
-			printf("AT ITERATION %d, NEW_STR SIZE WILL BE: %lu\n", i, strlen(new_str) + strlen(prefix[i]) + 1);
-			new_str = (char*)realloc(new_str, strlen(new_str) + strlen(prefix[i]) + 1);
-			sprintf(new_str, "%s%s", new_str, prefix[i]);
-		}
-		else {
-			printf("AT ITERATION %d, NEW_STR SIZE WILL BE: %lu\n", i, strlen(new_str) + strlen(prefix[i]) + 2);
-			new_str = (char*)realloc(new_str, strlen(new_str) + strlen(prefix[i]) + 2);
-			sprintf(new_str, "%s:%s", new_str, prefix[i]);
-		}
-		printf("NEW_STR SIZE IS: %lu\n", strlen(new_str) + 1);
-	}
+	append_prefixes(&new_str, prefix);
 
-	printf("\nSTRING COMPLETE\nIF LAST CHAR IS NULL, THEN YES WILL BE PRINT: ");
-	if (new_str[strlen(new_str)] == '\0') printf("YES\n");
-	else printf("%c", new_str[strlen(new_str)]);
-	printf("\t\t### %s\n", new_str);
+	write_to_pipe(new_str);
 
 	free(new_str);
 	return 0;
 }
+
 
 int agregar(char* prefix[], int level, char* path) {
 	if(!prefix) return -1;
 
 	char count[10];
 	sprintf(count, "%d", level);
-	printf("SIZE OF COUNT %lu\n", strlen(count));
 
 	char* new_str = (char*)calloc(
 		strlen(prefix[0]) +
@@ -66,67 +66,12 @@ int agregar(char* prefix[], int level, char* path) {
 		sizeof(char)
 		);
 
-	sprintf(new_str, "%s;2%s;%s;", prefix[0], count, path);
-	printf("SIZE EXPECTED: %lu\n", strlen(prefix[0]) + strlen(count) + 4);
-	printf("NEW_STR: %s WITH SIZE: %lu\n", new_str, strlen(new_str) + 1);
-	printf("IF LAST CHAR IS NULL, THEN YES WILL BE PRINT: ");
-	if (new_str[strlen(new_str)] == '\0') printf("YES\n");
-	else printf("%c", new_str[strlen(new_str)]);
+	sprintf(new_str, "%s;3%s;%s;", prefix[0], count, path);
 
-	for(int i = 1; prefix[i]; i++) {
-		if (i == 1) {
-			printf("AT ITERATION %d, NEW_STR SIZE WILL BE: %lu\n", i, strlen(new_str) + strlen(prefix[i]) + 1);
-			new_str = (char*)realloc(new_str, strlen(new_str) + strlen(prefix[i]) + 1);
-			sprintf(new_str, "%s%s", new_str, prefix[i]);
-		}
-		else {
-			printf("AT ITERATION %d, NEW_STR SIZE WILL BE: %lu\n", i, strlen(new_str) + strlen(prefix[i]) + 2);
-			new_str = (char*)realloc(new_str, strlen(new_str) + strlen(prefix[i]) + 2);
-			sprintf(new_str, "%s:%s", new_str, prefix[i]);
-		}
-		printf("NEW_STR SIZE IS: %lu\n", strlen(new_str) + 1);
-	}
+	append_prefixes(&new_str, prefix);
 
-	printf("\nSTRING COMPLETE\nIF LAST CHAR IS NULL, THEN YES WILL BE PRINT: ");
-	if (new_str[strlen(new_str)] == '\0') printf("YES\n");
-	else printf("%c", new_str[strlen(new_str)]);
-	printf("\t\t### %s\n", new_str);
+	write_to_pipe(new_str);
 
 	free(new_str);
 	return 0;
 }
-
-/*
-int agregar(char *prefix[], unsigned level, char *path){
-	if(!prefix) return -1;
-	char lvl[10];
-	sprintf(lvl, "%d", level);
-
-	char* new_str = (char*) calloc(
-		strlen( prefix[0] ) +
-		strlen( prefix[1] ) +
-		strlen(path) +
-		strlen(lvl) +
-		5,
-		sizeof(char)
-		); // 5 => ;, '\0' and "3"
-	sprintf(new_str, "%s;%s%s;%s;%s", prefix[0], "3", lvl, path, prefix[1]);
-
-	for(int i = 2; prefix[i]; i++){
-		new_str = (char*)realloc( new_str, strlen(new_str) + strlen( prefix[i] ) + 2 ); // 2 => ; and '\0'
-		sprintf(new_str, "%s:%s", new_str, prefix[i]);
-	}
-
-	int fd = open(SERVER_NAME, O_WRONLY);
-	printf(" !!! AGGREGATE OPPENED SERVER_NAME TO FD %d !!!\n", fd);
-	puts(new_str);
-
-	write(fd, new_str, strlen(new_str) + 1);
-
-	close(fd);
-
-	free(new_str);
-
-	return 0;
-}
-*/
