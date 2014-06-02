a27044c6b7cd36fdb32d304a823303afe5427117
Fernando Mendes <devfrmendes@gmail.com>
2014-06-02 23:48:58 +0100

    More comments

--- src/pipe_hash.h
+++ src/pipe_hash.h
@@ -9,6 +9,11 @@ void deletePipeTable(PipeTable pt);
 /*Creating a new Pipe Table*/
 PipeTable newPipeTable(int size);
 
+/** Checks if a district exists, creating it if so.
+  * ret shall contain the file descriptors created for father-child communication
+  * Returns 1 if needed to create a new father-son Pipe structure
+  * Returns 0 if it already existed
+  */
 int pipe_writer(PipeTable pt, char* name, int** ret);
 
 /**Sets the PID of the Pipe accessed by "name"*/
@@ -17,6 +22,10 @@ void set_pid(PipeTable pt, char*name, int pid);
 /**Closes every Pipe in the given PipeTable*/
 void shutdown_children(PipeTable pt);
 
+/** Iterates the hash searching for the element with given pid
+  * Removes it if found. In that case returns the name of the Pipe structure
+  * Otherwise returns NULL
+  */
 char* bury_dead_child(PipeTable pt, int pid);
 
 #endif
