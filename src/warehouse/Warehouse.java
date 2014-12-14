31744d96901dad8d268a4f52bd6d7c77db41777e
Fernando Mendes <devfrmendes@gmail.com>
2014-12-14 17:15:25 +0000

    Adds Warehouse stub

--- src/warehouse/Warehouse.java
+++ src/warehouse/Warehouse.java
@@ -1,5 +1,35 @@
 package warehouse;
 
-public class Warehouse {
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class Warehouse {
+    private Set<Task> tasks;
+    private Map<String, Integer> stock;
+    private ReentrantLock stockLock;
+    private ReentrantLock tasksLock;
+
+    public Warehouse() {
+        tasks = new HashSet<Task>();
+        stockLock = new ReentrantLock();
+        tasksLock = new ReentrantLock();
+    }
+
+    public void stockUp(String item, int quantity) {
+        // TODO
+    }
+
+    public int addTask(String name, Map<String, Integer> items) {
+        // TODO
+    }
+
+    public void startTask(int id) {
+        // TODO
+    }
+
+    public void endTask(int id) {
+        // TODO
+    }
 }
