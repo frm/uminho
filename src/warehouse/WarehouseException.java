9b54721b8ed7682b4a5ddb34605b1f72336b662b
Fernando Mendes <devfrmendes@gmail.com>
2014-12-14 17:30:32 +0000

    Adds custom exception handler

---
+++ src/warehouse/WarehouseException.java
@@ -0,0 +1,26 @@
+package warehouse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by frmendes on 12/14/14.
+ */
+public class WarehouseException extends Exception {
+    private static Map<String, String> EXCEPTIONS = new HashMap<String, String>() {{
+       this.put("InexistentTaskException", "You referenced a task that does not exist.");
+    }};
+
+    public WarehouseException() {
+        super();
+    }
+
+    public WarehouseException(String className) {
+        super(EXCEPTIONS.get(className));
+    }
+
+    public WarehouseException(String className, String message) {
+        super(EXCEPTIONS.get(className));
+        System.err.println(message);
+    }
+}
