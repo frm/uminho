9e29ccaf95957c377e7a5213346669e8946048b4
Fernando Mendes <devfrmendes@gmail.com>
2014-12-31 12:52:29 +0000

    Updates creation script, adds a seed script

---
+++ etc/habitat_seed.sql
@@ -0,0 +1,22 @@
+use `habitat`;
+
+INSERT INTO Funcionario
+    (Nome, DataNascimento, Morada, NIF, NIB,
+        Escolaridade, Nacionalidade, Naturalidade, EstadoCivil)
+    VALUES ("Habitat Employee", MAKEDATE(1980, 215), "Braga", "123456789", "1234-5678-9012-34",
+        "BSc", "Portuguese", "Fanecas de Cima", "Single");
+
+INSERT INTO Atividade
+    (Nome)
+    VALUES ('Graphic Designer');
+
+INSERT INTO Equipa
+    (Nome, FuncionarioResp)
+    VALUES ("DEFAULT", 1);
+
+INSERT INTO Voluntario
+    (Nome, Morada, NIF, NIB, DataNascimento, Nacionalidade, Naturalidade,
+        EstadoCivil, Observacoes, Escolaridade, AtividadeID, EquipaID)
+    VALUES("Alastair Reilly", "32nd Avenue, Bristol", "987654321", "0987-6543-2109-87", MAKEDATE(1984, 200),
+       "British", "London", "Married", "I found his name on Reddit", "MSc", 1, 1);
+
