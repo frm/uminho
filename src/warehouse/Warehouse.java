df7a2988c0dc30f47aa5dc7681026245d3a3bb39
Fernando Mendes <devfrmendes@gmail.com>
2015-01-03 20:24:41 +0000

    Implements requestMaterial with concurrency control and a flexible queueing system

--- src/warehouse/Warehouse.java
+++ src/warehouse/Warehouse.java
@@ -1,6 +1,7 @@
 package warehouse;
 
 import java.util.*;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Warehouse {
@@ -8,12 +9,20 @@ public class Warehouse {
     private Map<String, Item> stock;
     private ReentrantLock stockLock;
     private ReentrantLock taskTypesLock;
+    Condition turn;
+    PriorityQueue<Long> removeQueue;
+    long nextTicket;
+
+    private static final int REM_QUEUE_LIMIT = 4;
 
     public Warehouse() {
         taskTypes = new HashMap<>();
         stock = new HashMap<>();
         stockLock = new ReentrantLock();
         taskTypesLock = new ReentrantLock();
+        this.turn = stockLock.newCondition();
+        this.removeQueue = new PriorityQueue<Long>();
+        this.nextTicket = 1;
     }
 
     // TODO: can that lock mixup cause a deadlock?
@@ -128,35 +137,50 @@ public class Warehouse {
         type.unlock();
         return result;
     }
-    
+
     private void requestMaterial(Map<String, Integer> material) throws InexistentItemException, InterruptedException {
         stockLock.lock();
+
+        long myTurn = nextTicket++;
         Iterator<Map.Entry<String, Integer>> it = material.entrySet().iterator();
 
         while( it.hasNext() ) {
             Map.Entry<String, Integer> pair = it.next();
             Item i = stock.get(pair.getKey());
-
-            if(i == null)
-                throw new InexistentItemException("User requested " + pair.getKey());
+            boolean waited = false;
 
             while( !i.isAvailable( pair.getValue() ) ) {
                 stockLock.unlock();
                 i.waitForMore();
                 stockLock.lock();
+                waited = true;
+                it = material.entrySet().iterator();        // rewinding the iterator
+            }
+
+            if(waited)
+                continue;
+
+            Long next = removeQueue.peek();
+
+            while(next != null && myTurn > next.longValue() + REM_QUEUE_LIMIT) { // peek returns null if the queue is empty
+                removeQueue.add(myTurn);
+                this.turn.await();
+                removeQueue.remove(myTurn);
+                next = removeQueue.peek();
                 it = material.entrySet().iterator();        // rewinding the iterator
             }
         }
 
         for(Map.Entry<String, Integer> pair : material.entrySet()) {
             try {
-                stock.get(pair.getKey()).remove(pair.getValue());
+                stock.get( pair.getKey() ).remove( pair.getValue() );
             } catch (InvalidItemQuantityException e) {} // never occurs because the task validates its needs upon creation. we always remove valid values
         }
 
+        this.turn.signalAll();
         stockLock.unlock();
     }
-
+    
     private void returnMaterial(Map<String, Integer> material) throws InexistentItemException {
         stockLock.lock();
 
