ef89d10310976d0bba1d6c719746b3601d811712
Fernando Mendes <devfrmendes@gmail.com>
2015-01-04 21:21:34 +0000

    Adds documentation for WarehouseException

--- src/warehouse/WarehouseException.java
+++ src/warehouse/WarehouseException.java
@@ -7,6 +7,7 @@ import java.util.Map;
  * Created by frmendes on 12/14/14.
  */
 public class WarehouseException extends Exception {
+    // Map of messages to correspond all classes that extend WarehouseExtension
     private static Map<String, String> EXCEPTIONS = new HashMap<String, String>() {{
         this.put("InexistentTaskException", "You referenced a task that does not exist.");
         this.put("InexistentTaskTypeException", "You referenced a task type that does not exist.");
@@ -25,16 +26,30 @@ public class WarehouseException extends Exception {
         super();
     }
 
+    /**
+     *
+     * @param className name of the subclass
+     */
     public WarehouseException(String className) {
         super(EXCEPTIONS.get(className));
         this.className = className;
     }
 
+    /**
+     *
+     * @param className name of the subclass
+     * @param message message to be given for debug
+     */
     public WarehouseException(String className, String message) {
         super(EXCEPTIONS.get(className));
         this.className = className;
+        //System.err.println(message);
     }
 
+    /**
+     *
+     * @return message with the exception and the corresponding message
+     */
     public String getUserMessage(){
         return "[" + className + "] " + EXCEPTIONS.get(className);
     }
