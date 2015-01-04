ac2f0d42598797a16c85f0ba024b867dd680ff76
Fernando Mendes <devfrmendes@gmail.com>
2015-01-04 13:29:28 +0000

    Adds exception message

--- src/warehouse/WarehouseException.java
+++ src/warehouse/WarehouseException.java
@@ -10,7 +10,7 @@ public class WarehouseException extends Exception {
     private static Map<String, String> EXCEPTIONS = new HashMap<String, String>() {{
         this.put("InexistentTaskException", "You referenced a task that does not exist.");
         this.put("InexistentTaskTypeException", "You referenced a task type that does not exist.");
-        this.put("ExistentException", "The task you tried to add already exists");
+        this.put("ExistentTaskException", "The task you tried to add already exists");
         this.put("TaskNotRunningException", "The task can't be stopped because it's not being done by any client");
         this.put("NotSubscribedException", "That ID is not subscribed to that task");
         this.put("AlreadySubscribedException", "That ID is already subscribed to that task");
