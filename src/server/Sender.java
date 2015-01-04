3a958c4c2b52437c5711c2fd7af7a6e2424f2987
Bruno Ferreira <chalkos@chalkos.net>
2015-01-04 15:22:02 +0000

    Added subscriber so that only one thread writes to the piped output stream

---
+++ src/server/Sender.java
@@ -0,0 +1,60 @@
+package server;
+
+import packet.Packet;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Sender implements Runnable{
+    private Queue<Packet> objects;
+    private ReentrantLock objectsLock;
+    private Condition hasObjects;
+    private ObjectOutputStream out;
+
+    Sender(ObjectOutputStream o){
+        out = o;
+        objects = new LinkedList<>();
+        objectsLock = new ReentrantLock();
+        hasObjects = objectsLock.newCondition();
+    }
+
+    public void send(Packet p){
+        objectsLock.lock();
+
+        objects.add(p);
+
+        hasObjects.signalAll();
+        objectsLock.unlock();
+    }
+
+    @Override
+    public void run() {
+        Boolean streamOK = true;
+
+        while (streamOK) {
+            objectsLock.lock();
+            while (objects.isEmpty())
+                try {
+                    hasObjects.await();
+                } catch (InterruptedException e) {}
+
+            try {
+                out.writeObject(objects.remove());
+            } catch (IOException e) {
+                streamOK = false;
+            } finally {
+                objectsLock.unlock();
+            }
+        }
+
+    }
+}
+
+
+
+
+
