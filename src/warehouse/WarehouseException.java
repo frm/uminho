125270806890f0a9c42760f6d5d270985941703b
Fernando Mendes <devfrmendes@gmail.com>
2015-01-04 18:55:36 +0000

    Fixes #41

--- src/warehouse/WarehouseException.java
+++ src/warehouse/WarehouseException.java
@@ -32,7 +32,6 @@ public class WarehouseException extends Exception {
     public WarehouseException(String className, String message) {
         super(EXCEPTIONS.get(className));
         this.className = className;
-        System.err.println(message);
     }
 
     public String getUserMessage(){
