1ada85ca0708095114b05491bfa2fd3ba34c9bb5
Fernando Mendes <devfrmendes@gmail.com>
2015-01-05 11:53:01 +0000

    Adds interface code

---
+++ src/habitat/JTextAreaLimit.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package habitatinterface;
+
+import javax.swing.text.*;
+
+/**
+ *
+ * @author João
+ */
+public class JTextAreaLimit extends PlainDocument {
+    private int limit;
+    // optional uppercase conversion
+    private boolean toUppercase = false;
+     
+    JTextAreaLimit(int limit) {
+        super();
+        this.limit = limit;
+    }
+     
+    JTextAreaLimit(int limit, boolean upper) {
+        super();
+        this.limit = limit;
+        toUppercase = upper;
+    }
+     
+    public void insertString
+            (int offset, String  str, AttributeSet attr)
+            throws BadLocationException {
+        if (str == null) return;
+         
+        if ((getLength() + str.length()) <= limit) {
+            if (toUppercase) str = str.toUpperCase();
+            super.insertString(offset, str, attr);
+        }
+    }
+}
