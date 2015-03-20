268c492a7525265da4552060bacb6aa5c488fd7d
Fernando Mendes <devfrmendes@gmail.com>
2015-03-20 19:17:56 +0000

    Completes string tree

--- 02_enamex/tree.c
+++ 02_enamex/tree.c
@@ -3,56 +3,74 @@
 #include <string.h>
 
 typedef struct node {
+    struct node* left;
+    struct node* right;
     char* content;
-    struct node *left;
-    struct node *right;
-} node, *tree;
+} *node;
 
-tree* new_node(char* content) {
-    tree *new;
-    *new = (tree)malloc( sizeof(struct node) );
-    (*new) -> content = strdup(content);
-    (*new) -> left = NULL;
-    (*new) -> right = NULL;
-    return new;
+char* clone_content(char* content) {
+    return strdup(content);
 }
 
-int _add_to(tree* t, tree n) {
-    if(*t == NULL) {
+node new_node(char* content) {
+    node n = NULL;
+    n = (node)malloc( sizeof(struct node) );
+    n -> left = NULL;
+    n -> right = NULL;
+    n -> content = clone_content(content);
+    return n;
+}
+
+int compare(char* c1, char* c2) {
+    return strncmp(c1, c2, strlen(c1));
+}
+
+void collision(node t, node n) {
+    printf("COLISION\n");
+    return;
+}
+
+int __add_to(node* t, node n) {
+    if(!*t) {
         *t = n;
         return 1;
     }
 
-    int r = strncmp( (*t) -> content, n -> content, strlen((*t) -> content + 1) );
+    int r = compare(n -> content, (*t) -> content);
 
-    if (r == 0)
+    if(r == 0) {
+        collision(*t, n);
         return 0;
+    }
     else if (r > 0)
-        return _add_to(&( (*t) -> right), n);
+        return __add_to(& (*t) -> right, n);
     else
-        return _add_to(&( (*t) -> left), n);
+        return __add_to(& (*t) -> left, n);
 }
 
-int add_to(tree *t, char* content) {
-    return _add_to(t, *new_node(content));
+int add_to(node* t, char* content) {
+    return __add_to(t, new_node(content));
 }
 
-int print_tree(tree t, int i) {
-    printf("### NODE %d:\nCONTENT: %s\n\n", ++i, t -> content);
-    i = print_tree(t -> left, i + 1);
-    i = print_tree(t -> right, i + 1);
-    return i + 1;
+void print_tree(node t) {
+    if(!t) return;
+    print_tree(t -> left);
+    printf("%s\n", t -> content);
+    print_tree(t -> right);
 }
-/*
+
+node* new_tree() {
+    node* n = (node*)malloc(sizeof(node));
+    *n = NULL;
+    return n;
+}
+
 int main() {
-    tree *t = NULL;
-    t = new_node("BRAGA");
-    printf("YO\n");
+    node* t = new_tree();
+    add_to(t, "BRAGA");
+    add_to(t, "GUIMARAES");
     add_to(t, "ALCANTARA");
-    add_to(t, "PORTO");
-    add_to(t, "LISBOA");
-    print_tree(*t, 0);
+    add_to(t, "BRAGA");
+    print_tree(*t);
     return 0;
 }
-*/
-
