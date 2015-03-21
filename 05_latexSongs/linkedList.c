1f06fcd2e4dbd2231de86ab6259b999045aa56b6
tiagoddinis <tiagoddinis@hotmail.com>
2015-03-21 23:12:44 +0000

    Added latexSongs (almost done)

---
+++ 05_latexSongs/linkedList.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct song{
+	char *title;
+	char *from;
+	char *author;
+	char *lyrics;
+	char *music;
+	char *singer;
+	char *lyricsText;
+	struct song *next;
+} *SongP, Song;
+
+typedef struct songList {
+    struct song *cursor;
+} *SongListP, SongList;
+
+SongListP init(){
+	SongListP r = malloc (sizeof(SongList));
+	r->cursor = NULL;
+	return r;
+}
+
+SongP newSong(char *info[7]){
+	int stringSize = 0;
+	SongP newSong = (SongP) malloc (sizeof(Song));
+
+	if(info[0] != NULL) newSong->title = strdup(info[0]);
+	if(info[1] != NULL) newSong->from = strdup(info[1]);
+	if(info[2] != NULL) newSong->author = strdup(info[2]);
+	if(info[3] != NULL) newSong->lyrics = strdup(info[3]);
+	if(info[4] != NULL) newSong->music = strdup(info[4]);
+	if(info[5] != NULL) newSong->singer = strdup(info[5]);
+	if(info[6] != NULL) newSong->lyricsText = strdup(info[6]);
+	
+	newSong->next = NULL;
+	return newSong;
+}
+
+/*SongP newSong(char *info[6]){
+	int stringSize = 0;
+	SongP newSong = (SongP) malloc (sizeof(Song));
+
+	stringSize = strlen(ttl) + 1;
+	newSong->title = malloc(sizeof(char) * stringSize);
+	strncpy(newSong->title, ttl, stringSize);
+	
+	newSong->next = NULL;
+	return newSong;
+}*/
+
+int addSong(SongListP sl, char *info[7]){
+	SongP ns = newSong(info);
+	ns->next = sl->cursor;
+	sl->cursor = ns;
+	return 1;
+}
+/*
+int main(){
+	SongListP mySongs = init();
+	addSong(mySongs,"juju");
+	addSong(mySongs,"falafel");
+	while(mySongs->cursor != NULL){
+		printf("%s\n",mySongs->cursor->title );
+		mySongs->cursor = mySongs->cursor->next;
+	}
+}
+*/
