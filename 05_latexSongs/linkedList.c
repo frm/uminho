2075a241a12e6ee3b1d4b735eb9110d83ecd7535
tiagoddinis <tiagoddinis@hotmail.com>
2015-03-25 19:04:59 +0000

    Fixes

--- 05_latexSongs/linkedList.c
+++ 05_latexSongs/linkedList.c
@@ -23,26 +23,13 @@ SongListP init(){
 	return r;
 }
 
-SongP newSong(char *info[7]){
-	int stringSize = 0;
+SongP newSong(){
 	SongP newSong = (SongP) malloc (sizeof(Song));
-
-	if(info[0] != NULL) newSong->title = strdup(info[0]);
-	if(info[1] != NULL) newSong->from = strdup(info[1]);
-	if(info[2] != NULL) newSong->author = strdup(info[2]);
-	if(info[3] != NULL) newSong->lyrics = strdup(info[3]);
-	if(info[4] != NULL) newSong->music = strdup(info[4]);
-	if(info[5] != NULL) newSong->singer = strdup(info[5]);
-	if(info[6] != NULL) newSong->lyricsText = strdup(info[6]);
-	
 	newSong->next = NULL;
 	return newSong;
 }
 
-int addSong(SongListP sl, char *info[7]){
-
-	SongP ns = newSong(info);
-	
+int addSong(SongListP sl, SongP ns){
 	ns->next = sl->cursor;
 	sl->cursor = ns;
 	return 1;
