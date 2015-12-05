09bc0fd90c7a537fc52916c4b0f2e4d9eefe71f8
Fernando Mendes <devfrmendes@gmail.com>
2015-12-05 20:37:33 +0000

    Adds admin logic to user

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -2,6 +2,8 @@ package models;
 
 import co.paralleluniverse.actors.ActorRef;
 
+import java.util.Date;
+
 /**
  * Created by frm on 28/11/15.
  */
@@ -10,10 +12,13 @@ public class User {
     private String password;
     private boolean logged; // TODO: can this be replaced by checking if the connection is null?
     private ActorRef connection;
+    private boolean admin;
+    private Date adminTimestamp;
 
     public User(String uname, String password) {
         this.uname = uname;
         this.password = password;
+        this.admin = false;
     }
 
     public boolean login(String password, ActorRef address) {
@@ -37,6 +42,40 @@ public class User {
         }
 
         return b;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public boolean authorityOver(User u) {
+        return (
+                this.admin
+                && u.admin
+                && this.adminTimestamp.before(u.adminTimestamp)
+                );
+    }
+
+    public boolean addAdminPrivileges() {
+        boolean b = admin;
+
+        if(!admin) {
+            admin = true;
+            adminTimestamp = new Date();
+        }
+
+        return !b;
+    }
+
+    public boolean revokeAdminPrivileges() {
+        boolean b = admin;
+
+        if(admin) {
+            admin = false;
+            adminTimestamp = null;
+        }
+
+        return !b;
     }
 
     @Override
