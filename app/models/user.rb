1e8fb318fb0b24f790977990c263ef8ae5fba724
Fernando Mendes <devfrmendes@gmail.com>
2016-04-21 12:43:24 +0100

    Updates user and movie to include reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :name,  presence: true, length: { maximum: 75 }
   validates :bio,   length: { maximum: 300 }
+
+  has_many :reviews
 end
