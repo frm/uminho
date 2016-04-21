f5a3d1cb2f3cbab8c8784423f44d5cff3c5d3f91
Fernando Mendes <devfrmendes@gmail.com>
2016-04-21 12:43:57 +0100

    Adds Review controller and simple views to display the movie's reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = current_user.reviews.create(
+                review_params.merge(movie_id: params[:movie_id]))
+    redirect_to movie_url(params[:movie_id])
+  end
+
+  def destroy
+    Review.find(params[:id]).destroy
+    redirect_to movie_url(params[:movie_id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:score, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
 
-  resources :movies, only: [:index, :show]
+  resources :movies, only: [:index, :show] do
+    resources :reviews, only: [:create, :destroy]
+  end
 end
