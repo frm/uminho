4eda9064e30ee070e9d16904499f33f11f60390a
Fernando Mendes <devfrmendes@gmail.com>
2016-04-21 12:43:07 +0100

    Adds review model spec

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  before do
+    @user = FactoryGirl.create :user
+    @review = @user.reviews.build FactoryGirl.attributes_for(:review, movie_id: 1)
+  end
+
+  subject { @review }
+
+  describe "basic review" do
+    it { should be_valid }
+  end
+
+  describe "required fields" do
+    describe "when user is blank" do
+      before { @review.user = nil }
+      it { should_not be_valid }
+    end
+
+    describe "when movie is blank" do
+      before { @review.movie = nil }
+      it { should_not be_valid }
+    end
+
+    describe "when score is blank" do
+      before { @review.score = nil }
+      it { should_not be_valid }
+    end
+
+    describe "when description is blank" do
+      before { @review.description = nil }
+      it { should be_valid }
+    end
+  end
+
+  describe "field limit" do
+    describe "when description is too long" do
+      before { @review.description = 'a' * 1001 }
+      it { should_not be_valid }
+    end
+  end
+
+  describe "score format" do
+    describe "when score is below 0" do
+      before { @review.score = -1.0 }
+      it { should_not be_valid }
+    end
+
+    describe "when score is over 10" do
+      before { @review.score = -1.0 }
+      it { should_not be_valid }
+    end
+
+    describe "when format isn't in halves" do
+      before { @review.score = 1.2 }
+      it { should_not be_valid }
+    end
+  end
+end
