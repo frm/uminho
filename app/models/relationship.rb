6e548710f5c9a5e349088f11fa06fbb39058a107
Fernando Mendes <devfrmendes@gmail.com>
2016-06-21 11:47:13 +0100

    Adds Following tracking

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,10 @@
 class Relationship < ActiveRecord::Base
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user },
+    only: [:create]
+  has_many :activities, as: :trackable,
+    class_name: 'PublicActivity::Activity', dependent: :destroy
+
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
 
