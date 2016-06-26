065ea453ad5769adcf26d9f3e8b152cc014b8dda
Fernando Mendes <devfrmendes@gmail.com>
2016-06-26 22:06:23 +0100

    Adds more quotes

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,7 +4,38 @@ $(function() {
     beginning: "It's funny how the colors of the real world",
     punchline: 'only seem really real when you watch them on a screen',
     author: 'Anthony Burgess'
-  }];
+  },
+  {
+    beginning: "Cinema is a matter of what's in the frame",
+    punchline: "and what's out.",
+    author: 'Martin Scorsese'
+  },
+  {
+    beginning: "I have a theory that movies operate",
+    punchline: "on the level of dreams where you dream yourself",
+    author: "Meryl Streep"
+  },
+  {
+    beginning: "Art reflects society. Cinema doesn't dictate - ",
+    punchline: "you portray what the society demands",
+    author: "Sonam Kapoor"
+  },
+  {
+    beginning: "Dialogue should simply be a sound among other sounds, just something that comes",
+    punchline: "out of the mouths of people whose eyes tell the story in visual terms",
+    author: "Alfred Hitchcock"
+  },
+  {
+    beginning: "There are no rules in filmmaking. Only sins.",
+    punchline: "And the cardinal sin is dullness",
+    author: "Frank Capra"
+  },
+  {
+    beginning: "There's nothing creative",
+    punchline: "about living within your means",
+    author: "Francis Ford Coppola"
+  }
+  ];
 
   var quote = quotes[Math.floor(Math.random() * quotes.length)];
 
