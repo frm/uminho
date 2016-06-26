39352e3ef9dd937335b154724547fb3e613d6c1c
Fernando Mendes <devfrmendes@gmail.com>
2016-06-26 18:52:08 +0100

    Adds random cinemagraph to the landing page

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,22 @@
 module WelcomeHelper
+  def random_cinemagraph
+    cinemagraph = cinemagraphs.sample
+    asset_path cinemagraph_path(cinemagraph)
+  end
+
+  private
+
+  def cinemagraphs
+    blacklist = [".", ".."]
+    cinemagraphs = Dir.glob("app/assets/images/cinemagraphs/*")
+    blacklist.each do |blacklisted|
+      cinemagraphs.delete(blacklisted)
+    end
+
+    cinemagraphs
+  end
+
+  def cinemagraph_path(cinemagraph)
+    cinemagraph.split(File::SEPARATOR)[-2..-1].join(File::SEPARATOR)
+  end
 end
