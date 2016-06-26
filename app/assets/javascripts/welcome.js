a8cee678804765a843c0524451056fd676daf3d3
Fernando Mendes <devfrmendes@gmail.com>
2016-06-26 21:51:58 +0100

    Adds working landing page

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,23 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function() {
+  var quotes = [
+  {
+    beginning: "It's funny how the colors of the real world",
+    punchline: 'only seem really real when you watch them on a screen',
+    author: 'Anthony Burgess'
+  }];
+
+  var quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  var landingQuote = $('.landing-quote');
+  landingQuote.append(quote.beginning + ' ');
+  landingQuote.append("<span>" + quote.punchline + "</span>");
+
+  // var words = quote.punchline.split(' ');
+
+  // words.forEach(function(w, i) {
+  //   landingQuote.append("<span>" + w + " </span>");
+  // });
+
+  landingQuote.append("<span class='landing-quote-author'>"
+      + " - " + quote.author + "</span>");
+});
