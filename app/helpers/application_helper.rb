51c2a9d4606fc1683665b5ce1b86c164560cabc5
Fernando Mendes <devfrmendes@gmail.com>
2016-06-28 13:34:51 +0100

    Fixes flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,17 @@
 module ApplicationHelper
   def bootstrap_class_for(flash_type)
-    { success: "alert-success",
-      error: "alert-danger",
-      alert: "alert-warning",
-      notice: "alert-info" }[flash_type] || flash_type.to_s
+    f = flash_type.to_sym
+    { "success" => "alert-success",
+      "error" => "alert-danger",
+      "alert" => "alert-warning",
+      "notice" => "alert-info" }[flash_type] || flash_type
   end
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(
         content_tag(:div, message,
-                    class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+                    class: "flash-message alert #{bootstrap_class_for(msg_type)} fade in") do
           concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
           concat message
         end
