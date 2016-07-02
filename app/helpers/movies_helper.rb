69ae244e5d3756b40cc331fd68399f14b9797a01
Fernando Mendes <devfrmendes@gmail.com>
2016-07-02 10:03:53 +0100

    Prettifies movie rating on show

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,12 @@
 module MoviesHelper
+  def movie_badge_color(movie)
+    case movie.rating
+    when 0..2.0
+      "red"
+    when 2.5..3.5
+      "blue"
+    when 4..5
+      "green"
+    end
+  end
 end
