5862dd5bc71ccc1dd193b815c9a08793d219bd57
Fernando Mendes <devfrmendes@gmail.com>
2016-07-02 16:00:59 +0100

    Fixes review voting response partials

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,7 +2,7 @@ class RatingsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @review = Review.find(params[:review_id])
+    @review = Review.find(params[:review_id]).decorate
     @review.vote_by voter: current_user, vote: params[:review_rating]
     respond_to do |format|
       format.html { redirect_to @review.movie }
@@ -11,7 +11,7 @@ class RatingsController < ApplicationController
   end
 
   def destroy
-    @review = Review.find(params[:review_id])
+    @review = Review.find(params[:review_id]).decorate
     @review.unvote_by current_user
     respond_to do |format|
       format.html { redirect_to @review.movie }
